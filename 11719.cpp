09ed62ad3083a706f502f92ac0245f4dad4129bd
[minD3D] <gomdori626@gmail.com>
2017-06-21 00:02:32 +0900

    add

---
+++ 11719.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	int n = 0;
+	while (1) {
+		n++;
+		getline(cin, str);
+
+		if (atoi(str.c_str()) == EOF | n > 100)
+			break;
+
+		cout << str << endl;
+	}
+
+}
